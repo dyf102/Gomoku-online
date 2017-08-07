12b0eb91813f5076c8bb29ede9cd32c31a3e38dc
duanyuwei <duanyuwei@corp.netease.com>
2017-08-07 06:27:12 -0700

    TODO: fix game list do not show bug Changes to be committed: 	modified:   client/controller/game_controller.py 	modified:   client/view/gamelist_widget.py

--- client/controller/game_controller.py
+++ client/controller/game_controller.py
@@ -4,7 +4,7 @@ import sys
 import logging
 import json as JSON
 from network import Client
-from PyQt4.QtCore import SIGNAL, QObject
+from PyQt4.QtCore import SIGNAL, QObject, QString
 from basecontroller import BaseController
 
 SERVICE_NAME = 'GameService'
@@ -15,6 +15,7 @@ class GameController(BaseController):
 
     def __init__(self):
         BaseController.__init__(self, service_name=SERVICE_NAME)
+        self.game_list = None
 
     def add_polling_game_list_task(self):
         client = self.get_client()
@@ -29,4 +30,9 @@ class GameController(BaseController):
         if data and data.get('code') == 200:
             game_list = data.get('list')
             logging.debug("get_game_list_callback %s", game_list)
-
+            self.game_list = game_list
+            for game in game_list:
+                txt = '{} vs {}'.format(game.get('host_name'), game.get('guest_name'))
+                logging.debug("----- %s", txt)
+                self.emit(SIGNAL('addItem(QString)'),
+                          QString('{} vs {}'.format(game.get('host_name'), game.get('guest_name'))))
--- client/view/gamelist_widget.py
+++ client/view/gamelist_widget.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 from PyQt4.QtGui import QListWidget, QMessageBox
 from PyQt4.QtCore import *
+from controller.chat_controller import ChatController
 
 
 class GameListWidget(QListWidget):
     def __init__(self, parent=None):
         QListWidget.__init__(self, parent)
         self.game_list = []
-        for i in range(10):
-            self.addItem(str(i))
+        # for i in range(10):
+        #    self.addItem(str(i))
         self.itemClicked.connect(self.Clicked)
+        self.connect(ChatController(), SIGNAL('addItem(QString)'), self.addItem)
 
     def Clicked(self, item):
         QMessageBox.information(self, "ListWidget", "You clicked: " + item.text())
+
